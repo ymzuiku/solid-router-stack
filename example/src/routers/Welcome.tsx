3f71c180e04bd93b7119a89a35bf91d01e7a665f
pillar <ymblender@gmail.com>
2022-08-25 03:44:00 +0800

    fix: update svg

--- example/src/routers/Welcome.tsx
+++ example/src/routers/Welcome.tsx
@@ -1,14 +1,11 @@
 import { Component, createSignal, For, mergeProps } from "solid-js";
 import { tw } from "twind";
+import Github from "./github.svg";
 
 import { createPropsSignal, setNavigationAnimation } from "solid-router-stack";
-import logo from "../logo.svg";
+import Logo from "../logo.svg";
 import { buttonCss } from "./classlist";
 import { routers } from "./routers";
-
-const github = `<svg height="100%" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="100%" data-view-component="true" class="octicon octicon-mark-github v-align-middle">
-    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" fill="currentColor"></path>
-</svg>`;
 
 const Welcome: Component<{ name: string; stackTop: boolean }> = (p) => {
   p = mergeProps({ name: "" }, p);
@@ -22,10 +19,11 @@ const Welcome: Component<{ name: string; stackTop: boolean }> = (p) => {
       >
         <a
           class={tw`text-white w-20 h-20 mb-10`}
-          ref={(r) => (r.innerHTML = github)}
           target="_blank"
           href="https://github.com/ymzuiku/solid-router-stack"
-        ></a>
+        >
+          <Github />
+        </a>
         <h1>Change navigation animation type:</h1>
         <select
           value={getMove()}
@@ -56,12 +54,12 @@ const Welcome: Component<{ name: string; stackTop: boolean }> = (p) => {
         <p class={tw`mt-10`}>Scroll view and go back page</p>
         <For each={Array(10).fill(0)}>
           {() => (
-            <img
-              src={logo}
+            <div
               class={tw`animate-spin duration-500 w-40 h-40 mx-auto`}
               style={{ "animation-duration": "8s" }}
-              alt="logo"
-            />
+            >
+              <Logo />
+            </div>
           )}
         </For>
         <button
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
-  plugins: [solidPlugin({ dev: true })],
+  plugins: [solidPlugin({ dev: true }), solidSvg()],
   server: {
     host: "0.0.0.0",
     port: 3000,
